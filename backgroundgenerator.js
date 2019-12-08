a54fbe96e77338dc2b76dcc6f39127c8e74e6938
swapnali12deokar <swapnalideokar0307@gmail.com>
2019-12-08 16:48:54 +0530

    sending background

---
+++ backgroundgenerator.js
@@ -0,0 +1,18 @@
+var css=document.querySelector('h3');
+var color1 = document.querySelector('.color1');
+var color2 =document.querySelector('.color2');
+var body = document.getElementById('gradient');
+
+//color1.addEventListener("input",setGradient);
+//color2.addEventListener("input",setGradient);
+function setGradient()
+
+{
+	
+	body.style.background= "linear-gradient(to right, "+ color1.value +" ," + color2.value+ ")";
+     console.log(color1.value);
+     console.log(color2.value);
+     css.textContent=body.style.background +"; " ;
+
+}
+
